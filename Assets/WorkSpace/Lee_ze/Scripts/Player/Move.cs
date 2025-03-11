16e5129cd8509bd00745cb02e1573b3ae3c00ca2
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-03-11 16:51:29 +0900

    [Feat]View rotation + relative movement to camera

--- Assets/WorkSpace/Lee_ze/Scripts/Player/Move.cs
+++ Assets/WorkSpace/Lee_ze/Scripts/Player/Move.cs
@@ -6,12 +6,10 @@ using UnityEngine.InputSystem;
 public class Move : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed = 3f;
+    private float moveSpeed = 0.01f;
 
     [SerializeField]
     private Rigidbody rb;
-
-    private Vector3 moveDir;
 
     [SerializeField]
     private RotateModel modelRotator;
@@ -19,15 +17,27 @@ public class Move : MonoBehaviour
     [SerializeField]
     private Animator playerAnim;
 
+    private Vector2 direction;
+
     private void FixedUpdate()
     {
-        if (moveDir != Vector3.zero)
+        Vector3 camForward = Camera.main.transform.forward;
+        
+        camForward.y = 0;
+
+        Vector3 camRight = Camera.main.transform.right;
+
+        camRight.y = 0;
+
+        if (direction != Vector2.zero)
         {
+            Vector3 moveDir = (camRight * direction.x + camForward * direction.y).normalized;
+
             Vector3 move = moveSpeed * Time.fixedDeltaTime * new Vector3(moveDir.x, 0, moveDir.z);
 
             rb.MovePosition(transform.position + move);
 
-            modelRotator.SetTargetDirection(moveDir);
+            modelRotator.SetTargetDirection(move);
 
             playerAnim.SetBool("IsWalking", true);
         }
@@ -41,13 +51,11 @@ public class Move : MonoBehaviour
     {
         if (ctx.phase == InputActionPhase.Performed)
         {
-            Vector2 direction = ctx.ReadValue<Vector2>();
-
-            moveDir = new Vector3(direction.x, 0, direction.y);
+            direction = ctx.ReadValue<Vector2>();
         }
         else if (ctx.phase == InputActionPhase.Canceled)
         {
-            moveDir = Vector3.zero;
+            direction = Vector2.zero;
 
             rb.velocity = Vector3.zero;
         }
