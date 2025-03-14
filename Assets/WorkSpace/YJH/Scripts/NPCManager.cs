ea485669c5379c845dfa1fb2f6e04109031a0ffb
jamjamstone <junghyun4545@naver.com>
2025-03-14 11:40:58 +0900

    [Chore] spawnmechanism fix
    스폰포인트를 이용한 npc 무작위 배치로 매커니즘 변경

--- Assets/WorkSpace/YJH/Scripts/NPCManager.cs
+++ Assets/WorkSpace/YJH/Scripts/NPCManager.cs
@@ -22,19 +22,42 @@ public class NPCManager : MonoBehaviour
         npcList = new List<GameObject>();
         npcScriptList = new List<NPC>();
         npcSpawnList = new List<GameObject>();
-
+        SetSpawnPoint();//초기 스폰 메커니즘 -> 나중에 완성도를 끌어올릴때 다른 로직을 사용해 보자 -> 단점으로는 밀도가 높아져 빈 공간이 생길 수 밖에 없음
         pool.SetPrefab(npc);
-        InitialSet();
+        //InitialSet();
+        InitialSetBySpawnPoint();
+        
         //pool.NPCS.Get();//NPC 생성
 
     }
-
+    public void SetSpawnPoint()
+    {
+        for (int i = 0; i < spawnGroup.transform.childCount; i++)//스폰 포인트 
+        {
+            npcSpawnList.Add(spawnGroup.transform.GetChild(i).gameObject);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
     
+    public void InitialSetBySpawnPoint()
+    {
+        CreateAllNPC();
+        foreach (NPC npc in npcScriptList)//npc들을
+        {
+            SetNPCTransform(npc.gameObject, npcSpawnList[Random.Range(0, npcSpawnList.Count)].transform.position);//랜덤하게 위치 설정
+
+        }
+    }
+
+
+
+
+
+
     public void InitialSet()//맵 리셋시 NPC 뿌리기 
     {
         CreateAllNPC();//최초 숫자인 50개만큼 NPC 호출
@@ -45,6 +68,11 @@ public class NPCManager : MonoBehaviour
 
 
     }
+
+    
+
+
+
     
     public void SetNPCTransform(GameObject npc, Vector3 position)//y 좌표 1.09
     {
