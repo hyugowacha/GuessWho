150f6fe22ee64bb6b1bf470151bf45e6aae0b55f
hyugowacha <uena0506@gmail.com>
2025-03-18 18:31:56 +0900

    [Feat] item spawner 코드 작성중

---
+++ Assets/WorkSpace/CEA/Scripts/ItemSpawnPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    [Header("아이템 스폰 포인트")] private GameObject[] spawnPoints;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
