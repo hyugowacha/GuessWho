16b596bc83d0619e4a1103f47073dad1cc94caf2
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-03-25 14:07:53 +0900

    Revert "Merge branch 'Develope' into CEA"
    This reverts commit 3cc1ee35687c813286ca549563873c9c81be02be, reversing
    changes made to c22986ea7dca26f28a7a50c33b5857e8bdc37d00.

--- Assets/WorkSpace/YJH/Scripts/NPCState.cs
+++ Assets/WorkSpace/YJH/Scripts/NPCState.cs
@@ -7,11 +7,7 @@ using Photon.Pun;
 using Unity.VisualScripting;
 
 
-public enum NPCStateName
-{
-    Hit,Idle,Walk,None
 
-}
 public interface INPCState
 {
     
@@ -38,7 +34,6 @@ public class NPCMove : INPCState
     readonly int hashIdle = Animator.StringToHash("isIdle");
 
     //GameObject forTest;
-    [PunRPC]
     public void PlayAnimation()
     {
         //animator관련
@@ -47,7 +42,7 @@ public class NPCMove : INPCState
         npcAnimator.SetBool(hashMove, true);
         
     }
-    [PunRPC]
+
     public void StopAnimation()
     {
         //animator관련
@@ -56,7 +51,6 @@ public class NPCMove : INPCState
         selfAgent.isStopped = true;
 
     }
-    [PunRPC]
     public void EnterState(NPC npc)
     {
         nowNPC = npc;
@@ -123,7 +117,6 @@ public class NPCMove : INPCState
     //    StateAction();
     //    this.destination = destination;
     //}
-    [PunRPC]
     public void StateAction()
     {
         PlayAnimation();
@@ -177,24 +170,21 @@ public class NPCHit : INPCState
     readonly int hashIdle = Animator.StringToHash("isIdle");
 
     //private bool isEnd;
-    [PunRPC]
     public void PlayAnimation()
     {
         //animator관련
-        npcAnimator.SetBool(hashHit,true);
+        npcAnimator.SetTrigger(hashHit);
         npcAnimator.SetBool(hashIdle,true);
         npcAnimator.SetBool("isAnger", true);
     }
-    [PunRPC]
+
     public void StopAnimation()
     {
         //animator관련
         npcAnimator.SetBool("isAnger", false);
-        npcAnimator.SetBool(hashHit, false);
-
     }
 
-    [PunRPC]
+
     public void EnterState(NPC npc)
     {
         isHit = false;
@@ -208,7 +198,6 @@ public class NPCHit : INPCState
         //게임 매니저나 여타 피격시 시행될 기능
         isHit=true;
     }
-    [PunRPC]
     public void StateAction()
     {
         PlayAnimation();
@@ -216,14 +205,6 @@ public class NPCHit : INPCState
     }
     public bool CheckStateEnd()
     {
-        if (npcAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime == 1)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
         if (isHit == true)
         {
             StopAnimation();
@@ -251,17 +232,15 @@ public class NPCIdle : INPCState
     readonly int hashHit = Animator.StringToHash("isHit");
     readonly int hashMove = Animator.StringToHash("isMove");
     readonly int hashIdle = Animator.StringToHash("isIdle");
-    [PunRPC]
     public void PlayAnimation()
     {
         npcAnimator.SetBool(hashIdle, true);
         npcAnimator.SetBool(hashMove, false);
         npcAnimator.SetBool("isAnger", false);
-        npcAnimator.SetBool(hashHit, false);
         //animator관련
 
     }
-    [PunRPC]
+
     public void StopAnimation()
     {
         npcAnimator.SetBool(hashIdle, false);
@@ -269,7 +248,7 @@ public class NPCIdle : INPCState
         //animator관련
 
     }
-    [PunRPC]
+
     public void EnterState(NPC npc)
     {
         isEnd = false;
@@ -280,14 +259,14 @@ public class NPCIdle : INPCState
         npcAgent = (npc as TestingNPC).SelfAgent;
         npcAgent.isStopped = true;
     }
-    [PunRPC]
+
     public void StateAction()
     {
         //StartCoroutine(NPCIdleAnimationPlay());
         PlayAnimation();
     }
-
     
+
     public bool CheckStateEnd()
     {
         delaiedime += Time.deltaTime;
