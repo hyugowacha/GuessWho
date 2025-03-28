4a179c9118b821971bfc474aacf6ce74901457b6
hyugowacha <uena0506@gmail.com>
2025-03-28 17:15:57 +0900

    [Fix] stone 코드 일부 수정

--- Assets/WorkSpace/CEA/Scripts/StoneController.cs
+++ Assets/WorkSpace/CEA/Scripts/StoneController.cs
@@ -6,18 +6,20 @@ using UnityEngine.Rendering;
 public class StoneController : MonoBehaviour
 {
     PlayerControl player;
+    TestingNPC npc;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            player = other.GetComponent<PlayerControl>();
             player.GetHit();
             Destroy(this.gameObject);
         }
 
         else if (other.CompareTag("NPC"))
         {
-            
+            npc = other.GetComponent<TestingNPC>();   
         }
     }
 }
