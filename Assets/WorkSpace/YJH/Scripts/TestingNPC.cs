53bc5e0c018afddfea7794fd9ce40dc7af4a44ca
jamjamstone <junghyun4545@naver.com>
2025-03-31 10:26:58 +0900

    [Chore] edit npc script
    NPC 동기화가 순간이동처럼 보이는 것을 막기 위해서 동기화 정확도를 줄이는 대신 transform view를 사용해서 부드럽게 만듦

--- Assets/WorkSpace/YJH/Scripts/TestingNPC.cs
+++ Assets/WorkSpace/YJH/Scripts/TestingNPC.cs
@@ -430,15 +430,15 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-        }
-        else
-        {
-            transform.position=(Vector3)stream.ReceiveNext();
-            transform.rotation=(Quaternion)stream.ReceiveNext();
-        }
+        //if (stream.IsWriting)
+        //{
+        //    stream.SendNext(transform.position);
+        //    stream.SendNext(transform.rotation);
+        //}
+        //else
+        //{
+        //    transform.position=(Vector3)stream.ReceiveNext();
+        //    transform.rotation=(Quaternion)stream.ReceiveNext();
+        //}
     }
 }
