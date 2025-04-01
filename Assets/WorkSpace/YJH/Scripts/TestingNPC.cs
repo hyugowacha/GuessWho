774a24504dad6c94b9ca104ce8d8dca01e01e005
jamjamstone <junghyun4545@naver.com>
2025-04-01 11:38:28 +0900

    [Chore] 사용하지 않는 코드 제거
    주석처리된 코드와 사용하지 않는 코드 제거

--- Assets/WorkSpace/YJH/Scripts/TestingNPC.cs
+++ Assets/WorkSpace/YJH/Scripts/TestingNPC.cs
@@ -10,24 +10,27 @@ using ZL.Unity;
 public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
 {
 
-    //상태 패턴
-    //[SerializeField] GameObject tempDestination;// 랜덤 목적지 지정 시스템 만들기 전에 사용하는 임시 변수
+    
+    
     [SerializeField] NavMeshAgent selfAgent;
     [SerializeField] Collider selfCollider;
     [SerializeField] AudioSource hitSoundSource;
     
     private INPCState nowState;
-    //private NavMeshSurface gamefield;
-    //private bool haveToChangeState;//현재로서는 필요 없으나 게임매니저에 사용할 수도 있을거 같기에 선언해 놓음
-    //private bool hasHit = false;
+    
     private float hitPenaltyTime=0.3f;
     [SerializeField] float hitTime = 0;
-    //public GameObject forTest;//목적지 디버그용 완제품엔 필요 없음
+    
     public Animator animator;
-    //public string forDebug;
+   
     private Vector3 npcDestination=new Vector3();
     readonly int hashselfVel = Animator.StringToHash("selfVel");
-    public NavMeshAgent SelfAgent { get { return selfAgent; } set { selfAgent = value; } }
+    public NavMeshAgent SelfAgent
+    {
+        get { return selfAgent; }
+        set { selfAgent = value; }
+    }
+
     public INPCState NowState
     {
         get { return nowState; }
@@ -43,11 +46,7 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
     public override void OnEnable()
     {
         base.OnEnable();
-        if (PhotonNetwork.IsMasterClient == false)
-        {
-            //selfAgent.enabled = false;
-            
-        }
+        
     }
     public void SetNPCTransform(Vector3 transformTo)
     {
@@ -65,30 +64,23 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
         }
         else
         {
-            //Debug.Log("conn");
+            
             StartCoroutine (NPCAnimationControl());
             if (PhotonNetwork.IsMasterClient == true)
             {
                 if (Random.Range(0f, 1f) < 0.5f)//일부는 바로 이동 일부는 대기 
                 {
                     photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Idle, UnityEngine.Random.Range(0.2f, 1.0f));
-                    //nowState=;
-                    //nowState.EnterState(this);
-                    //Debug.Log("setidle");
+                    
                 }
                 else
                 {
                     photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Walk, 0.0f);
-                    //nowState = new NPCMove();
-                    //nowState.EnterState(this);
-                    // Debug.Log("setmove");
+                    
                 }
                 StartCoroutine(CheckState());
             }
-            else
-            {
-                //selfAgent.enabled = false;
-            }
+            
 
         }
     }
@@ -100,7 +92,7 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
     [PunRPC]
     public void ChangeState(NPCStateName stateName)
     {
-        //forDebug=stateName.ToString();
+        
         switch (stateName)
         {
             case NPCStateName.None:
@@ -203,26 +195,15 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
             {
                 if(nowState is NPCHit)
                 {
-                    //hitTime += Time.deltaTime;
-                    //if (hitPenaltyTime <= hitTime)
-                    //{
-                    //    
-                    //    (nowState as NPCHit).StopAnimation();
-                    //    photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Idle, UnityEngine.Random.Range(0.2f, 1.0f));
-                    //    //ChangeState(NPCStateName.Idle);//new NPCIdle());
-                    //    selfCollider.enabled = true;
-                    //    hitTime = 0;
-                    //}
+                    
                     
                 }else if (nowState.CheckStateEnd() == true)
                 {
-                    //haveToChangeState=true;
+                    
                     if(nowState is NPCIdle)
                     {
                         photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Walk, 0f);
 
-                        //ChangeState(NPCStateName.Walk);//new NPCMove());
-                        
 
                     }
                     else if(nowState is NPCMove)
@@ -230,26 +211,20 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
                         
                         if (Random.Range(0, 100) < 70)
                         {
-                            //Debug.Log("changetomove");
-                            photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Idle,0.3f);
-                            //Debug.Log(photonView.ViewID);
-                            //ChangeState(NPCStateName.Idle,true);
                             
-                            //ChangeState(NPCStateName.Walk);
+                            photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Idle,0.3f);
+                            
 
                         }
                         else
                         {
-                            //Debug.Log("stayidle");
+                            
                             photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Idle, UnityEngine.Random.Range(0.8f, 1.5f));
-                            //ChangeState(NPCStateName.Idle);
+                            
                         }
                     }
                 }
-                else
-                {
-                    //haveToChangeState=false;
-                }
+                
 
             }
 
@@ -291,9 +266,6 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
             photonView.RPC("ChangeState", Photon.Pun.RpcTarget.All, NPCStateName.Hit);//포톤일 때 콜백으로 마스터 클라이언트에 전달함
         }
 
-
-        //Debug.Log("gethit");
-        //haveToChangeState = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -301,9 +273,9 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
         if (other.transform.root.tag == "Player")
         {
             selfCollider.enabled = false;
-            Debug.Log("hit");
+            
             var player = other.transform.root.transform;//플레이어관련 변경 일어날 시에는 코딩 새로 해야 함
-            //Debug.Log("detectplayer");
+            ;
             transform.LookAt(player);
             
 
@@ -322,8 +294,7 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
     
     public override void OnMasterClientSwitched(Player newMasterClient)
     {
-        Debug.Log("master changed");
-        //base.OnMasterClientSwitched(newMasterClient);
+        
         if(PhotonNetwork.IsMasterClient)
         {
             SelfAgent.enabled = true;
@@ -332,7 +303,7 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
             {
                 (nowState as NPCMove).EnterState(this, npcDestination);
                 nowState.StateAction();
-                //(nowState as NPCMove).SetDestination(npcDestination);
+               
 
             }
         }
@@ -344,15 +315,6 @@ public class TestingNPC : MonoBehaviourPunCallbacks,IHittable,IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        //if (stream.IsWriting)
-        //{
-        //    stream.SendNext(transform.position);
-        //    stream.SendNext(transform.rotation);
-        //}
-        //else
-        //{
-        //    transform.position=(Vector3)stream.ReceiveNext();
-        //    transform.rotation=(Quaternion)stream.ReceiveNext();
-        //}
+       
     }
 }
